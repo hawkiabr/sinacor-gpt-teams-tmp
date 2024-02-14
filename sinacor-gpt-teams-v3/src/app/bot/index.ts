758b3279309dd32096ed4add99131c8a21f82b05
André Marques de Araújo <andre@hawkia.com.br>
2024-02-14 16:37:09 -0300

    Terceira versão (baseada em template padrão de projetos OpenAI).

---
+++ sinacor-gpt-teams-v3/src/app/bot/index.ts
@@ -0,0 +1,87 @@
+import {ActivityTypes, TurnContext} from 'botbuilder';
+import {Application} from '@microsoft/teams-ai';
+import {ApplicationTurnState} from '..';
+import {
+  resetConversationHistory,
+  getChatResponse,
+  getCitations,
+  getSupportingContent,
+  isExampleMessage,
+  renderCard,
+  sendAdaptiveCard,
+  replaceCitations,
+  createConversationHistory,
+  addMessageToConversationHistory,
+  convertCitations,
+} from '../shared/helpers';
+
+import responseCard from '../shared/cards/response.json';
+import welcomeCard from '../shared/cards/welcome.json';
+
+import {ResponseCard, WelcomeCard} from '../shared/types';
+import {constants} from '../shared/constants';
+
+const setup = (app: Application) => {
+  app.activity(
+    ActivityTypes.InstallationUpdate,
+    async (context: TurnContext) => {
+      const card = renderCard<WelcomeCard>(welcomeCard, {
+        questions: constants.questions,
+      });
+      await sendAdaptiveCard(context, card);
+    }
+  );
+
+  app.message(
+    'Novo chat',
+    async (context: TurnContext, state: ApplicationTurnState) => {
+      resetConversationHistory(state);
+      await context.sendActivity(
+        "Novo chat iniciado, mensagens anteriores não serão usadas como contexto para novas consultas."
+      );
+      const card = renderCard<WelcomeCard>(welcomeCard, {
+        questions: constants.questions,
+      });
+      await sendAdaptiveCard(context, card);
+    }
+  );
+
+  app.activity(
+    ActivityTypes.Message,
+    async (context: TurnContext, state: ApplicationTurnState) => {
+      if (isExampleMessage(context.activity)) resetConversationHistory(state);
+      const {text} = context.activity;
+
+      createConversationHistory(state);
+
+      addMessageToConversationHistory(state, {
+        content: text,
+        role: 'user',
+      });
+
+      const chatResponse = await getChatResponse(state.conversation.messages);
+      const chatContext = chatResponse.choices[0].context;
+      const {followup_questions} = chatContext;
+      const {data_points} = chatContext;
+      const {message: reply} = chatResponse.choices[0];
+
+      addMessageToConversationHistory(state, reply);
+
+      const citationFileReferences = getCitations(reply.content);
+      const answer = replaceCitations(citationFileReferences, reply.content);
+      const citations = convertCitations(citationFileReferences);
+      const supportingContent = getSupportingContent(data_points);
+
+      const data: ResponseCard = {
+        answer,
+        citations,
+        supportingContent,
+      };
+      const card = renderCard<ResponseCard>(responseCard, data);
+
+      await sendAdaptiveCard(context, card, followup_questions);
+    }
+  );
+};
+
+export {setup};
